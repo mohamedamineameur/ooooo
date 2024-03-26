b6548b20d1eb27985e994853bfaaf468541481fd
mohamedamineameur <134455223+mohamedamineameur@users.noreply.github.com>
2024-03-25 21:31:20 -0400

    Update Mailer.js

--- Controlers/Mailer.js
+++ Controlers/Mailer.js
@@ -1,6 +1,33 @@
 import nodemailer from 'nodemailer'
 
+export const mailer =  (mail, sub, txt)=>{
 
+    
+    let transporter = nodemailer.createTransport({
+        host: "smtp.sendgrid.net",
+        port: 465,
+        secure: true, // true pour le port 465, false pour d'autres ports
+        auth: {
+          user: "apikey",
+          pass: "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"
+        }
+      });
+      
+      let mailOptions = {
+        from: '"Mohamed Dev" <mohamed.amine.ameur@mohamed-amine-ameur.com>',
+        to: mail,
+        subject: sub,
+        text: txt,
+        html: `<p style="color: blue; font-size: 16px;">${txt}</p>`  // HTML body
+      };
+      
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log("Message envoyé: %s", info.messageId);
+      });
+}
 
 
 
